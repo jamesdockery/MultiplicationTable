9e7fba6ff4cc382fdee24e101d2b02cce3698981
James <jamesdockery011@gmail.com>
2021-02-19 09:59:37 -0500

    first commit

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] results = new int[10, 10];
+            for(int i = 0; i < results.GetLength(0); i++)
+            {
+                for(int j = 0; j < results.GetLength(1); j++)
+                {
+                    results[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            for(int i = 0; i < results.GetLength(0); i++)
+            {
+                for(int j = 0; j < results.GetLength(1); j++)
+                {
+                    Console.Write("{0,4}", results[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
